02d4ef5c858caea121ab67216155cf048e895a59
thiagorozaa <thiago.rozaa@gmail.com>
2020-10-28 18:13:27 -0300

    Initial Commit

---
+++ Gerador de Números/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gerador de Números
+//
+//  Created by Thiago Rosa Ataide on 28/10/20.
+//  Copyright © 2020 Thiago Rosa Ataide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
